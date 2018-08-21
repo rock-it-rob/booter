a2df154ed9cbb9299c81ba414c8b98d9ae4bf051
Rob Benton <rmbenton@arkbluecross.com>
2018-08-21 10:51:27 -0500

    Changing message to a bad one

--- src/main/java/com/rob/booter/web/controller/Index.java
+++ src/main/java/com/rob/booter/web/controller/Index.java
@@ -29,7 +29,7 @@ public class Index
     public ResponseEntity<String> get() throws JsonProcessingException
     {
         final HashMap<String, String> map = new HashMap<>();
-        map.put("status", "peachy");
+        map.put("status", "nasty message");
         final String result = objectMapper.writeValueAsString(map);
 
         return ResponseEntity.status(Response.SC_OK).body(result);

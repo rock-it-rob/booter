d390360b682721ad9ad1a99e432d5d98dcb8fd8c
Rob Benton <rob.benton7@gmail.com>
2018-09-04 08:53:26 -0500

    Changed status to 400. Set content type.

--- src/main/java/com/rob/booter/web/advisor/JsonProcessingExceptionAdvisor.java
+++ src/main/java/com/rob/booter/web/advisor/JsonProcessingExceptionAdvisor.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.rob.booter.web.response.ErrorResponse;
 import com.rob.booter.web.response.entity.JsonError;
 import org.eclipse.jetty.server.Response;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,7 +21,8 @@ public class JsonProcessingExceptionAdvisor
     public ResponseEntity<ErrorResponse> jsonProcessingException(JsonProcessingException e)
     {
         return ResponseEntity
-            .status(Response.SC_INTERNAL_SERVER_ERROR)
+            .status(Response.SC_BAD_REQUEST)
+            .contentType(MediaType.APPLICATION_JSON)
             .body(new ErrorResponse().addError(new JsonError(e)));
     }
 }

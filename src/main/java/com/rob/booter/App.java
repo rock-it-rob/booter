138edd448b78c92dc7b1c849067eaba686c718d3
Rob Benton <rob.benton7@gmail.com>
2018-09-07 07:45:26 -0500

    All tests work.
    I had to add profile support for the tests. This is because the
    method of obtaining the version number from inside the packaged
    jar file does not work in the maven verify phase.

--- src/main/java/com/rob/booter/App.java
+++ src/main/java/com/rob/booter/App.java
@@ -1,11 +1,11 @@
 package com.rob.booter;
 
-import com.rob.booter.meta.ReleaseVersion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 /**
+ * App is the entry point for the application.
+ *
  * @author Rob Benton
  */
 @SpringBootApplication
@@ -14,16 +14,5 @@ public class App
     public static void main(String[] args)
     {
         SpringApplication.run(App.class, args);
-    }
-
-    /**
-     * The release bean which returns version info.
-     *
-     * @return {@link ReleaseVersion}
-     */
-    @Bean
-    public ReleaseVersion releaseVersion()
-    {
-        return () -> getClass().getPackage().getImplementationVersion();
     }
 }
